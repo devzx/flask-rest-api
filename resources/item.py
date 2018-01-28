52c845005e16b0b200f3e594b60d8a16b8e7b47a
Eli Judah <elijudah@gmail.com>
2018-01-28 22:18:05 +0000

    Got rid of a useless class method for retrieving all items

--- resources/item.py
+++ resources/item.py
@@ -44,4 +44,4 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return {'items:': [item.json() for item in ItemModel.find_all()]}
+        return {'items:': [item.json() for item in ItemModel.query.all()]}
