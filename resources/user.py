eae1fc9dd55ec082d5e0aaef644a01b504dc80c2
Eli Judah <elijudah@gmail.com>
2018-01-28 22:16:35 +0000

    Added user model and resource model. Added JWT authentication via flask-jwt-simple

---
+++ resources/user.py
@@ -0,0 +1,41 @@
+from flask_restful import Resource
+from webargs import fields
+from webargs.flaskparser import use_args
+from flask_jwt_simple import create_jwt, jwt_required
+
+from models.user import UserModel
+
+
+class UserRegister(Resource):
+    allowed_args = {
+        'username': fields.String(required=True),
+        'password': fields.String(required=True),
+    }
+
+    @use_args(allowed_args)
+    def post(self, args):
+        if UserModel.find_by_username(args['username']):
+            return {'message': 'user already exists'}
+        user = UserModel(**args)
+        user.insert()
+        return {'message': 'user created'}, 201
+
+
+class UserLogin(Resource):
+    allowed_args = {
+        'username': fields.String(required=True),
+        'password': fields.String(required=True),
+    }
+
+    @use_args(allowed_args)
+    def post(self, args):
+        user = UserModel.find_by_username(args['username'])
+        if user and user.password == args['password']:
+            return {'jwt': create_jwt(identity=user.username)}
+        return {'message': 'access denied'}
+
+
+class UserList(Resource):
+    @jwt_required
+    def get(self):
+        return {'users': [user.json() for user in UserModel.query.all()]}
