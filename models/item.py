8c6e14f633a40864eaae73e9a45dd8baef87c012
Eli Judah <elijudah@gmail.com>
2018-01-30 15:35:38 +0000

    Created relationship with stores table and corrected tablename variable name

--- models/item.py
+++ models/item.py
@@ -2,15 +2,19 @@ from db import db
 
 
 class ItemModel(db.Model):
-    __tables__ = 'items'
+    __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
-    def __init__(self, name, price):
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
+    def __init__(self, name, price, store_id):
         self.name = name
         self.price = price
+        self.store_id = store_id
 
     @classmethod
     def find_by_name(cls, name):
