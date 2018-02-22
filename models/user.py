f48710e360afa5da0c1c195356f6d2c43d1cf362
Eli Judah <elijudah@gmail.com>
2018-02-22 19:46:51 +0000

    Fixed the __tablename__ variable name

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@ from db import db
 
 
 class UserModel(db.Model):
-    __tables__ = 'users'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
